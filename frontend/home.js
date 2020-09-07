88fc4689e8e87a35776ee4b0b3dad655d62b3667
chsohn15 <chsohn15@gmail.com>
2020-09-06 20:48:24 -0400

    add home page

---
+++ frontend/home.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded",function(){
+    const body = document.querySelector('body')
+    let room1Image = document.createElement('image')
+    
+    const button1 = document.getElementById('journeys-room-btn')
+
+    button1.addEventListener("click",function(){
+        document.write()
+    })
+})

16b37291c4dae0e8f43747e061dbed8be44a43e5
chsohn15 <chsohn15@gmail.com>
2020-09-09 23:47:20 -0400

    moved html to js file

--- frontend/sandbox/drawing/draw.js
+++ frontend/sandbox/drawing/draw.js
@@ -1,4 +1,37 @@
 // When true, moving the mouse draws on the canvas
+
+document.body.innerHTML = 
+`<h1>Create Your Own Artwork!</h1>
+<canvas id="myCanvas" width="560" height="360"></canvas>
+
+<select id="color-selector">
+    <option value="black">Black</option>
+    <option value="red">Red</option>
+    <option value="blue">Blue</option>
+    <option value="green">Green</option>
+    <option value="yellow">Yellow</option>
+    <option value="purple">Purple</option>
+</select>
+<select id="background-color-selector">
+    <option value="white">White</option>
+    <option value="#00BFFF">Sky Blue</option>
+    <option value="#90EE90">Light Green</option>
+    <option value="#FFFF99">Yellow</option>
+    <option value="#C71585">Violet Red</option>
+</select>
+<select id="thickness-selector">
+    <option value=1>1</option>
+    <option value=2>2</option>
+    <option value=3>3</option>
+    <option value=4>4</option>
+    <option value=5>5</option>
+    <option value=6>6</option>
+    <option value=7>7</option>
+    <option value=8>8</option>
+    <option value=9>9</option>
+    <option value=10>10</option>
+</select>`
+
 let drawing = false;
 let x = 0;
 let y = 0;
@@ -11,9 +44,9 @@ const context = myCanvas.getContext('2d');
 // Add the event listeners for mousedown, mousemove, and mouseup
 function drawLine(context, x1, y1, x2, y2) {
 
-    const color = document.querySelector('#color-picker').value
-    const thickness = document.querySelector('#thickness-picker').value
-    const backgroundColor = document.querySelector('#background-color-picker').value
+    const color = document.querySelector('#color-selector').value
+    const thickness = document.querySelector('#thickness-selector').value
+    const backgroundColor = document.querySelector('#background-color-selector').value
     //debugger
     const canvas = document.getElementById('myCanvas')
     canvas.style.background = backgroundColor
